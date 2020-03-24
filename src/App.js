7b065b2480eeb245240c890076dfb669ec0fbb6c
Joelson Reis <50884925+joelsondsbreis@users.noreply.github.com>
2020-03-23 22:32:15 -0300

    Init style page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import Table from './Table';
 
 import Form from './Formulary';
